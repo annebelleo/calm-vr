4270f1199d86ee582659bebeab2e6defe3c30a6c
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2018-02-25 03:38:54 +0000

    i mean the clouds appear but that's about it

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Cloud Watching VR
-The repo name is misleading. AthenaHacks 2018
+The repo name is misleading. Built with A-Frame and Three.js. AthenaHacks 2018
--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-var max = 10;
-var min = 3;
-var size = Math.random() * (max - min) + min;
-var positionX = Math.floor(Math.random() * 5);
-var positionY = Math.floor(Math.random() * 5);
-var positionZ = Math.floor(Math.random() * -5);
-
-var points = `${positionX} ${positionY} ${positionZ}`;
-var colors = ["white", "FloralWhite", "GhostWhite", "Ivory", "DarkGray", "DarkGrey", "LightGray", "LightGrey"] // white -> gray
-var index = Math.floor(Math.random() * (colors.length));
+var size = Math.random()
+var positionX;
+var positionY;
+var positionZ;
+var points;
+var colors = ["#ffffff", "#f2f2f2", "#e6e6e6", "#d9d9d9", "#cccccc"]; // white -> gray
+var index;
 
 function appendCloud() {
     // creating a new sphere
     for (var i = 0; i < 10; i++) {
+        positionX = Math.random() * 7 + 3;
+        positionY = Math.floor(Math.random() * 5);
+        positionZ = Math.random() * (-10 || 10) + -10;
+        index = Math.floor(Math.random() * (colors.length));
+        points = `${positionX} ${positionY} ${positionZ}`;
         var newCircle = document.createElement("a-circle");
         newCircle.setAttribute("color", colors[index]);
         newCircle.setAttribute("radius", `${size}`);
         newCircle.setAttribute("position", `${points}`);
-        newCircle.addEventListener("click", function(){this.setAttribute("visible", true)});
+        newCircle.addEventListener("click", function(){
+            this.setAttribute("visible", true);
+        });
         document.getElementById("scene").appendChild(newCircle);
-        console.log(newCircle);
     }
- 
 }
 
 // .catch(function (err) {
