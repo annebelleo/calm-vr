ab0ba1226df0acc1060c8a7fd97148daa44de438
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2018-02-25 01:20:26 +0000

    intro screen

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-var max = 10
-var min = 3
+var max = 10;
+var min = 3;
+var size = Math.random() * (max - min) + min;
+var positionX = Math.floor(Math.random() * 5);
+var positionY = Math.floor(Math.random() * 5);
+var positionZ = Math.floor(Math.random() * -5);
 
-function appendSphere() {
+var points = `${positionX} ${positionY} ${positionZ}`;
+var colors = ["white", "FloralWhite", "GhostWhite", "Ivory", "DarkGray", "DarkGrey", "LightGray", "LightGrey"] // white -> gray
+var index = Math.floor(Math.random() * (colors.length));
+
+function appendCloud() {
     // creating a new sphere
-    for (var i = 0; i < Math.random() * 50; i++) {
-        var size = Math.random() * (max - min) + min;
-        var positionX = Math.floor(Math.random() * 5);
-        var positionY = Math.floor(Math.random() * 5);
-        var positionZ = Math.floor(Math.random() * -5);
+    for (var i = 0; i < 10; i++) {
         var newCircle = document.createElement("a-circle");
-        newCircle.setAttribute("color", "white");
-        // preferably a scale of diff shades of white to gray
+        newCircle.setAttribute("color", colors[index]);
         newCircle.setAttribute("radius", `${size}`);
-        newCircle.setAttribute("position", `${positionX} ${positionY} ${positionZ}`);
+        newCircle.setAttribute("position", `${points}`);
+        newCircle.addEventListener("click", function(){this.setAttribute("visible", true)});
         document.getElementById("scene").appendChild(newCircle);
+        console.log(newCircle);
     }
+ 
 }
 
 // .catch(function (err) {
